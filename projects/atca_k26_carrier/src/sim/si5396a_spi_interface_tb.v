9af6faacf0d0fab5102a64af6c32dced024a4ffe
joao-o <joao.s.oliveira@tecnico.ulisboa.pt>
2024-08-12 18:49:11 +0100

    pll communications now working, adc deserializer only works with ila? investigate, looked at chopper and it seems like it works, underflow? check fifo resetting

---
+++ projects/atca_k26_carrier/src/sim/si5396a_spi_interface_tb.v
@@ -0,0 +1,53 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/02/2024 04:30:54 PM
+// Design Name: 
+// Module Name: si5396a_spi_interface_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module si5396a_spi_writer_tb;
+
+    reg clk,reset;
+    wire nCS,sdo,sck,readdata,in_en;
+    
+    si53xx_spi_interface dut (.clk(clk),
+                              .reset(reset),
+                              .read(1'b1),
+                              .write(1'b0),
+                              .rw_addr(8'haa),
+                              .write_data(8'ha6),
+                              .nCS(nCS),
+                              .sdo(sdo),
+                              .sclk(sck),
+                              .sdi(1'b1),
+                              .rom_data(16'h1616),
+                              .read_data(readdata),
+                              .in_en(in_en));                     
+                              
+    
+    initial begin
+      clk = 1'b0;
+      forever clk = #5 ~clk;
+     end
+     
+    initial begin
+      reset = 1'b1;
+      #100 reset = 1'b0;
+    end
+    
+endmodule
